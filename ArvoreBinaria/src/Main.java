4961f61f89d4353f504dd7321196a1b24245af90
ZeFurtado <lukhasfurtado9@gmail.com>
2026-05-20 19:17:35 -0300

    Adicionado mais nodos á árvore

--- ArvoreBinaria/src/Main.java
+++ ArvoreBinaria/src/Main.java
@@ -4,7 +4,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         
-        Nodo node_Raiz = new Nodo(50);
-        node_Raiz.setValorEsquerda(new Nodo(20));
+        ArvoreBinaria avBinary = new ArvoreBinaria();
+        
+        avBinary.Inserir(50); //Raiz
+        avBinary.Inserir(20); //Valor á esquerda da raiz
+        avBinary.Inserir(60); //Valor á direita da raiz
+        avBinary.Inserir(25); //Valor á direita do nodo 20
+        avBinary.Inserir(70); //Valor á direita do nodo 60
+        avBinary.Inserir(55); //Valor á esquerda do nodo 60
+        
+        avBinary.EmOrdem();
     }
 }
