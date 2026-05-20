cfeac4d35f302dfec51d24558be6b1840279b3bf
ZeFurtado <lukhasfurtado9@gmail.com>
2026-05-20 17:36:41 -0300

    Criado método de inserção na árvore

--- ArvoreBinaria/src/ArvoreBinaria.java
+++ ArvoreBinaria/src/ArvoreBinaria.java
@@ -6,11 +6,21 @@ public class ArvoreBinaria {
     }
     
     public void Inserir(int valor){
-        this.raiz = InserirRecursivo(valor);
+        this.raiz = InserirRecursivo(this.raiz, valor);
     }
 
-    private Nodo InserirRecursivo(int valor){
-        return new Nodo(valor);
+    private Nodo InserirRecursivo(Nodo r, int valor){
+        if(r == null){
+            return new Nodo(valor);
+        }else{
+            if(valor > r.getValor()){
+                r.setValorDireita(InserirRecursivo(r.getValorDireita(), valor));
+            }else if(valor <= r.getValor()){
+                r.setValorEsquerda(InserirRecursivo(r, valor));
+            }
+        }
+        
+        return r;
     }
     
 }
