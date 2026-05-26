c6f60464bcb4164af9f3d773e9497f964d66fb7b
ZeFurtado <lukhasfurtado9@gmail.com>
2026-05-25 21:56:45 -0300

    Adicionado comentários explicando o funcionamento do método PercorreEmOrdem()

--- ArvoreBinaria/src/ArvoreBinaria.java
+++ ArvoreBinaria/src/ArvoreBinaria.java
@@ -29,15 +29,22 @@ public class ArvoreBinaria {
         PercorreEmOrdem(this.raiz);
     }
     
+    //Percurso em Ordem (LVR) verificar todos o nodos a esquerda do nodo raiz
+    //caso não haja nenhum nodo á esquerda ele imprime o nodo raiz e continua
+    //a busca á direita do nodo raiz
     private Nodo PercorreEmOrdem(Nodo r){
-        if(r == null) return null;
+        if(r == null) return null; //Caso nodo seja vazio retorna nulo para 
+                                    //encerrar a recursão
         
+        //Caso nodo não exista á esquerda imprime o valor e continua a busca
+        //á direita do nodo
         if(PercorreEmOrdem(r.getValorEsquerda()) == null){
-            System.out.println(r.getValor());
+            System.out.println(r.getValor()); 
             return PercorreEmOrdem(r.getValorDireita());
         }
         
         return r;
     }
+    
     
 }
