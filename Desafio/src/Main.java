736bfa5645c281839419f667b24cd1058684e793
ZeFurtado <lukhasfurtado9@gmail.com>
2026-06-05 13:56:09 -0300

    Classe Main

--- Desafio/src/Main.java
+++ Desafio/src/Main.java
@@ -1,5 +1,16 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Out");
+        ArvoreAVL avBinary = new ArvoreAVL();
+
+        avBinary.Inserir(50); //Raiz
+        avBinary.Inserir(20); //Valor á esquerda da raiz
+        avBinary.Inserir(60); //Valor á direita da raiz
+        avBinary.Inserir(25); //Valor á direita do nodo 20
+        avBinary.Inserir(70); //Valor á direita do nodo 60
+        avBinary.Inserir(55); //Valor á esquerda do nodo 60
+
+        System.out.println("Percorre pré ordem");
+        avBinary.PreOrdem();
+        
     }
 }
